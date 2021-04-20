61d93fd790020ef947e9f26054d7eaea7d2d201d
HardCorePawn <hardcorepawn69@gmail.com>
2021-04-20 21:42:20 +1200

    Update dump_unenc_keys.py
    dumps generated addresses with keys to make searching easier

--- dump_unenc_keys.py
+++ dump_unenc_keys.py
@@ -3,9 +3,11 @@ import hashlib
 import codecs
 import mmap
 import sys
+import ecdsa
+import base58
 
-# Define base58
-def base58(address_hex):
+# Define base58str
+def base58str(address_hex):
     alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
     b58_string = ''
     # Get the number of leading zeros
@@ -23,6 +25,37 @@ def base58(address_hex):
     for one in range(ones):
         b58_string = '1' + b58_string
     return b58_string
+
+def priv_to_addr(address_hex,compressed):
+    signing_key = ecdsa.SigningKey.from_string(address_hex, curve=ecdsa.SECP256k1)
+    verifying_key = signing_key.get_verifying_key()
+    if not compressed:
+        public_key = bytes.fromhex("04") + verifying_key.to_string()
+    else:
+        public_key_x = verifying_key.to_string()[0:32]
+        public_key_y = verifying_key.to_string()[32:]
+        
+        if int(binascii.hexlify(public_key_y),16) % 2 == 0:
+            public_key = bytes.fromhex("02") + public_key_x
+        else:
+            public_key = bytes.fromhex("03") + public_key_x
+    
+    #hash sha256 of pubkey
+    sha256_1 = hashlib.sha256(public_key)
+    
+    #hash ripemd of sha of pubkey
+    ripemd160 = hashlib.new("ripemd160")
+    ripemd160.update(sha256_1.digest())
+    
+    #checksum
+    hashed_public_key = bytes.fromhex("00") + ripemd160.digest()
+    checksum_full = hashlib.sha256(hashlib.sha256(hashed_public_key).digest()).digest()
+    checksum = checksum_full[:4]
+    bin_addr = hashed_public_key + checksum
+    
+    #encode address to base58_check and return
+    result_addr = base58.b58encode(bin_addr).decode("utf-8")
+    return result_addr
 
 
 def main():
@@ -44,7 +77,9 @@ def main():
     for key in privkeyList:
         
         PKuncomp = b'80'+ binascii.hexlify(key)
+        uncomp_address = priv_to_addr(key,False)
         PKcomp = PKuncomp + b'01'
+        comp_address = priv_to_addr(key,True)
         
         PKuncomp_sha256_1 = hashlib.sha256(codecs.decode(PKuncomp, 'hex'))
         PKuncomp_sha256_2 = hashlib.sha256(PKuncomp_sha256_1.digest())
@@ -60,11 +95,11 @@ def main():
         comp_PK = PKcomp + checksum_comp
         #print(comp_PK)
         
-        WIF_uncomp = base58(uncomp_PK.decode("utf-8"))
-        WIF_comp = base58(comp_PK.decode("utf-8"))
+        WIF_uncomp = base58str(uncomp_PK.decode("utf-8"))
+        WIF_comp = base58str(comp_PK.decode("utf-8"))
         
-        print(WIF_uncomp)
-        print(WIF_comp)
+        print(uncomp_address + ":" + WIF_uncomp)
+        print(comp_address + ":" + WIF_comp)
 
 if __name__ == "__main__":
     main()
